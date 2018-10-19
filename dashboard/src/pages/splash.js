a40826605ca7b298f997782b4d7188e6cd2ef455
Aditya Vaidyam <avaidyam@purdue.edu>
2018-10-19 15:07:36 -0400

    [dashboard] Sample ORY Editor.

--- dashboard/src/pages/splash.js
+++ dashboard/src/pages/splash.js
@@ -12,6 +12,87 @@ import IconButton from '@material-ui/core/IconButton';
 import { LAMPSplash } from '../components/lamp_icons';
 import Markdown from "../components/markdown";
 
+// The editor core
+import Editor, { Editable, createEmptyState } from 'ory-editor-core'
+import 'ory-editor-core/lib/index.css' // we also want to load the stylesheets
+
+// The default ui components
+import { Trash, DisplayModeToggle, Toolbar as Toolbar2 } from 'ory-editor-ui'
+import 'ory-editor-ui/lib/index.css'
+
+// The rich text area plugin
+import slate from 'ory-editor-plugins-slate'
+import 'ory-editor-plugins-slate/lib/index.css'
+
+// The spacer plugin
+import spacer from 'ory-editor-plugins-spacer'
+import 'ory-editor-plugins-spacer/lib/index.css'
+
+// The image plugin
+import { imagePlugin } from 'ory-editor-plugins-image'
+import 'ory-editor-plugins-image/lib/index.css'
+
+// The video plugin
+import video from 'ory-editor-plugins-video'
+import 'ory-editor-plugins-video/lib/index.css'
+
+// The parallax plugin
+import parallax from 'ory-editor-plugins-parallax-background'
+import 'ory-editor-plugins-parallax-background/lib/index.css'
+
+// The html5-video plugin
+import html5video from 'ory-editor-plugins-html5-video'
+import 'ory-editor-plugins-html5-video/lib/index.css'
+
+// The native handler plugin
+import native from 'ory-editor-plugins-default-native'
+
+// The divider plugin
+import divider from 'ory-editor-plugins-divider'
+
+// Renders json state to html, can be used on server and client side
+import { HTMLRenderer } from 'ory-editor-renderer'
+
+const fakeImageUploadService = (defaultUrl) => (file, reportProgress) => {
+	return new Promise((resolve, reject) => {
+		let counter = 0
+		const interval = setInterval(() => {
+			counter++
+			reportProgress(counter * 10);
+			if (counter > 9) {
+				clearInterval(interval)
+				alert('This is a fake image upload service, please provide actual implementation via plugin properties')
+				resolve({ url: defaultUrl })
+			}
+		}, 500)
+	})
+}
+
+// Define which plugins we want to use. We only have slate and parallax available, so load those.
+const plugins = {
+	content: [slate(), spacer, imagePlugin({ imageUpload: fakeImageUploadService('/images/react.png') }), video, divider, html5video],
+	layout: [
+		parallax({ defaultPlugin: slate() }),
+	],
+
+	// If you pass the native key the editor will be able to handle native drag and drop events (such as links, text, etc).
+	// The native plugin will then be responsible to properly display the data which was dropped onto the editor.
+	native
+}
+
+// Creates an empty editable
+const content = createEmptyState()
+
+// Instantiate the editor
+const editor = new Editor({
+	plugins,
+	defaultPlugin: slate(),
+	// pass the content state - you can add multiple editables here
+	editables: [content],
+})
+editor.trigger.mode.edit()
+
+
 const rootURL = 'https://api.github.com/repos/BIDMCDigitalPsychiatry/LAMP-Docs/contents'
 const baseURL = 'https://raw.githubusercontent.com/BIDMCDigitalPsychiatry/LAMP-docs/master'
 const docPages = async () => {
@@ -73,6 +154,14 @@ class Splash extends React.Component {
 			<Markdown imageBaseUrl={baseURL} onPageEnter={(x) => this.props.history.push('/docs/' + x)}>
 				{this.docContents()}
 			</Markdown>
+		</Card>
+		<Card style={{ padding: 20, height: 900, margin: '0vh 10vw 5vh 10vw' }}>
+			<div className="ory-ui-editor">
+				<Editable editor={editor} id={content.id} />
+				<Trash editor={editor}/>
+				<DisplayModeToggle editor={editor}/>
+				<Toolbar2 editor={editor}/>
+			</div>
 		</Card>
 	</div>
 }
