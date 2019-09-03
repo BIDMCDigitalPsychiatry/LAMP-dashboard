c8bf01c834b049c493719b63a5cf37e981729060
Aditya Vaidyam <avaidyam@bidmc.harvard.edu>
2019-09-03 12:49:39 -0400

    Fix a few minor package issues.

--- src/components/Utils.js
+++ src/components/Utils.js
@@ -18,7 +18,7 @@ import IconButton from '@material-ui/core/IconButton'
 import DeleteIcon from '@material-ui/icons/Delete'
 import FilterListIcon from '@material-ui/icons/FilterList'
 import Divider from '@material-ui/core/Divider'
-import { useTheme } from '@material-ui/styles'
+import { useTheme } from '@material-ui/core/styles'
 import useMediaQuery from '@material-ui/core/useMediaQuery'
 
 // Convert underscore case into human-readable strings.
