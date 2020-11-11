a414ffc3fe6e8e363d76aabd5015362992b93f6d
Saritha <sarithapillai@hq.microobjects.net>
2020-11-11 15:28:02 +0530

    Translation files

---
+++ src/i18n.js
@@ -0,0 +1,20 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import Backend from 'i18next-http-backend';
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(Backend)
+  .init({
+    backend: {
+      preload: true,
+      // for all available options read the backend's repository readme file
+      loadPath: '/locales/{{lng}}/{{ns}}.json'
+    },
+    react: {
+      useSuspense: false,
+    },
+    keySeparator: false
+  });
+  
+export default i18n;
