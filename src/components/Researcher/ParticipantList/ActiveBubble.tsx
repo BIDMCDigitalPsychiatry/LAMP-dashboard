c70ea6733fdc2c7a46b14ffc10505637564eb9c5
Saritha <sarithapillai@hq.microobjects.net>
2021-04-07 17:14:44 +0530

    Active bubble change with never - remove commented console

--- src/components/Researcher/ParticipantList/ActiveBubble.tsx
+++ src/components/Researcher/ParticipantList/ActiveBubble.tsx
@@ -71,7 +71,6 @@ export default function Active({ participant, ...props }) {
   }
   return (
     <span>
-      {/* {console.log(dateInfo(participant.id).relative)} */}
       {dateInfo(participant.id).relative !== "in NaN years" &&
       dateInfo(participant.id).relative !== undefined &&
       dateInfo(participant.id).relative !== 0 ? (
