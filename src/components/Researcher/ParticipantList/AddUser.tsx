06c6eedba7d1e50788827d894c225dee1da1f5b1
Saritha <sarithapillai@hq.microobjects.net>
2021-06-29 09:32:26 +0530

    Package checking with dockerfile changes

--- src/components/Researcher/ParticipantList/AddUser.tsx
+++ src/components/Researcher/ParticipantList/AddUser.tsx
@@ -137,7 +137,7 @@ export default function AddUser({
     }
     setSelectedStudy("")
     closePopUp(3)
-    props.onClose()
+    props.onClose as any
   }
 
   const createNewStudy = () => {
@@ -172,7 +172,7 @@ export default function AddUser({
     }
     setSelectedStudy("")
     closePopUp(3)
-    props.onClose()
+    props.onClose as any
   }
 
   return (
