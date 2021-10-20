6bb7c4cfeed484560ef10c5e733b731190335e95
unknown <saritha.pillai@zcocorporation.com>
2021-10-20 19:21:36 +0530

    Safari issue - design fix - revert

--- src/components/PreventSkills.tsx
+++ src/components/PreventSkills.tsx
@@ -188,10 +188,6 @@ const useStyles = makeStyles((theme: Theme) =>
       overflow: "auto",
       [theme.breakpoints.down("xs")]: {
         maxWidth: "340px",
-        minWidth: 340,
-      },
-      [theme.breakpoints.up("sm")]: {
-        minWidth: "570px",
       },
     },
   })
