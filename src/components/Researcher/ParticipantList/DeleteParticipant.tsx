db91d548f3129f9cbab56e89e460b24705d29d38
unknown <saritha.pillai@zcocorporation.com>
2021-11-02 15:42:34 +0530

    delete participant credentials on deleting participant

--- src/components/Researcher/ParticipantList/DeleteParticipant.tsx
+++ src/components/Researcher/ParticipantList/DeleteParticipant.tsx
@@ -41,6 +41,12 @@ export default function DeleteParticipant({ participants, setParticipants, setUp
       })
       for (let participant of participants) {
         await LAMP.Participant.delete(participant.id)
+        LAMP.Credential.list(participant.id).then((cred) => {
+          cred = cred.filter((c) => c.hasOwnProperty("origin"))
+          cred.map((each) => {
+            LAMP.Credential.delete(participant.id, each["access_key"])
+          })
+        })
         await LAMP.Type.setAttachment(participant.id, "me", "lamp.name", null)
         Service.updateCount("studies", participant.study_id, "participant_count", 1, 1)
       }
