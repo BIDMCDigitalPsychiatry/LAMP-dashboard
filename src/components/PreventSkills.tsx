d59f88f42003862de99d104cb003ae59daa63acd
unknown <saritha.pillai@zcocorporation.com>
2021-11-02 18:13:52 +0530

    Text change on DBT skills- removed style change

--- src/components/PreventSkills.tsx
+++ src/components/PreventSkills.tsx
@@ -176,7 +176,7 @@ const useStyles = makeStyles((theme: Theme) =>
       },
     },
     skillWidth: { maxWidth: "100px" },
-    skillsContainer: { width: "100%", maxWidth: 570, minWidth: 570 },
+    skillsContainer: { width: "100%", maxWidth: 570 },
     accSummary: { paddingLeft: 0, paddingRight: 0 },
     greentxt: { color: "#21a521" },
     colCheck: { borderLeft: "0.5px solid #f4f4f4" },
