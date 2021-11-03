64ad231eb03dec636292a78cfb8dce06528671e8
Luke Scheuer <lukess@gmail.com>
2021-11-03 12:05:46 -0400

    removing some unnecessary editor style updates that are currently unneeded

--- src/components/data_portal/Editor.tsx
+++ src/components/data_portal/Editor.tsx
@@ -1,18 +1,3 @@
 import MonacoEditor from "react-monaco-editor"
-import { editor } from "monaco-editor"
-
-editor.defineTheme("myTheme", {
-  base: "vs",
-  inherit: true,
-  rules: [],
-  colors: {
-    "editor.foreground": "#000000",
-    //'editor.background': "#E8E8E8",
-    "editorCursor.foreground": "#7599FF",
-    //'editor.lineHighlightBackground': '#7599FF',
-    "editorLineNumber.foreground": "#7599FF",
-  },
-})
-editor.setTheme("myTheme")
 const Editor = MonacoEditor
 export default Editor
