c15b3a94f0d68750d2086e7a4e1ea4945b738f93
unknown <saritha.pillai@zcocorporation.com>
2021-11-17 20:25:43 +0530

    Tab update fix

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -851,7 +851,6 @@ export default function Feed({
                       className={feed.completed ? classes[feed.group + "Completed"] : classes[feed.group]}
                       variant="outlined"
                       onClick={() => {
-                        console.log(feed)
                         if (
                           !feed.completed &&
                           feed.clickable &&
