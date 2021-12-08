832fe79473a3f295c831e8361a78393092257eec
unknown <saritha.pillai@zcocorporation.com>
2021-12-08 20:43:10 +0530

    Activity tab none changes

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -856,8 +856,7 @@ export default function Feed({
                           feed.clickable &&
                           (!feed.activityData?.category ||
                             (!!feed.activityData?.category &&
-                              !!feed.activityData?.category[0] &&
-                              (feed.activityData?.category[0] || "") !== "")) &&
+                              feed.activityData?.category.length !== 0)) &&
                           ((["hourly", "every3h", "every6h", "every12h", "custom"].includes(feed.repeat_interval) &&
                             feed.time >= new Date().getTime()) ||
                             (!["hourly", "every3h", "every6h", "every12h"].includes(feed.repeat_interval) &&
