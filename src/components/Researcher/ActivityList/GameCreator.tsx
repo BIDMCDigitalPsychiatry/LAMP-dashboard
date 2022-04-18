09e6a9ffb101bec029f69dfe16c5b5f560572d8d
unknown <saritha.pillai@zcocorporation.com>
2022-04-18 09:50:30 +0530

    Design fix - #533

--- src/components/Researcher/ActivityList/GameCreator.tsx
+++ src/components/Researcher/ActivityList/GameCreator.tsx
@@ -26,7 +26,7 @@ import BreatheIcon from "../../../icons/Breathe.svg"
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    containerWidth: { maxWidth: 1055, marginBottom: "80px" },
+    containerWidth: { maxWidth: 1055, marginBottom: "10px" },
     backdrop: {
       zIndex: theme.zIndex.drawer + 1,
       color: "#fff",
