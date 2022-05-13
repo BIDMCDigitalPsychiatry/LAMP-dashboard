7e8a7e1808eca553f9bb4e84208a44fa04c75914
Facundo Almeida <falmeida@orangeloops.com>
2022-05-13 20:17:43 -0300

    Use i18next-browser-languagedetector for language setting

--- src/i18n.js
+++ src/i18n.js
@@ -1,16 +1,19 @@
 import i18n from "i18next"
 import { initReactI18next } from "react-i18next"
 import Backend from "i18next-http-backend"
+import LanguageDetector from "i18next-browser-languagedetector"
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .use(Backend)
+  .use(LanguageDetector)
   .init({
     backend: {
       preload: true,
       // for all available options read the backend's repository readme file
       loadPath: "/locales/{{lng}}/{{ns}}.json",
     },
+    supportedLngs: ["en", "es", "hi"],
     react: {
       useSuspense: false,
     },
