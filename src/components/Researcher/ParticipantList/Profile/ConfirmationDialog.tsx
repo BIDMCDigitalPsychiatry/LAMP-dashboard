4394bb1a060d1253032381e00e1d159a3da7ae31
ITHQ1947\saritha.pillai <saritha.pillai@zcocorporation.com>
2022-11-25 19:41:17 +0530

    Translation QA fixes

--- src/components/Researcher/ParticipantList/Profile/ConfirmationDialog.tsx
+++ src/components/Researcher/ParticipantList/Profile/ConfirmationDialog.tsx
@@ -42,12 +42,12 @@ export default function ConfirmationDialog({
                 "This sensor will be deleted for all the participants under this group. Are you sure you want to proceed?"
               )}`
             : confirmationDialog === 5
-            ? `${t("Are you sure you want to delete this sensor?.")}`
+            ? `${t("Are you sure you want to delete this sensor?")}`
             : confirmationDialog === 6
             ? `${t("Are you sure you want to delete this Activity?.")}`
             : confirmationDialog === 7
-            ? `${t("Are you sure you want to delete this Participant?.")}`
-            : `${t("Are you sure you want to delete this care team member? ")}`}
+            ? `${t("Are you sure you want to delete this Participant?")}`
+            : `${t("Are you sure you want to delete this care team member?")}`}
         </DialogContentText>
       </DialogContent>
       <DialogActions>
