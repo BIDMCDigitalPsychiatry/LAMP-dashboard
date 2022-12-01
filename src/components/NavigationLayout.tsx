5a0453c9279e027464c3fa82517a917371988d57
ITHQ1947\saritha.pillai <saritha.pillai@zcocorporation.com>
2022-12-01 18:29:00 +0530

    Patient replacing with user

--- src/components/NavigationLayout.tsx
+++ src/components/NavigationLayout.tsx
@@ -228,11 +228,12 @@ export default function NavigationLayout({
   const [researcherId, setResId] = useState(null)
   useEffect(() => {
     let isMounted = true
+    console.log(title, changeResearcherType)
     if (isMounted) {
       if (
         (authType === "researcher" || authType === "admin") &&
         typeof title != "undefined" &&
-        title.startsWith("User")
+        title.startsWith("Patient")
       ) {
         Service.getAll("researcher").then((researcher) => {
           setResId(researcher[0]["id"])
@@ -334,7 +335,7 @@ export default function NavigationLayout({
         <AppBar classes={{ root: classes.appbarResearcher }}>
           {(authType === "researcher" || authType === "admin") && (
             <Toolbar className={classes.logResearcherToolbar}>
-              {typeof title != "undefined" && title.startsWith("User") ? (
+              {typeof title != "undefined" && title.startsWith("Patient") ? (
                 <Box>
                   <IconButton className={classes.backbtn} onClick={participantBack} color="default" aria-label="Menu">
                     <Icon>arrow_back</Icon>
@@ -353,7 +354,7 @@ export default function NavigationLayout({
                       aria-label="Menu"
                       style={{
                         marginLeft:
-                          supportsSidebar && typeof title != "undefined" && title.startsWith("User") ? 0 : undefined,
+                          supportsSidebar && typeof title != "undefined" && title.startsWith("Patient") ? 0 : undefined,
                       }}
                     >
                       <Icon>arrow_back</Icon>
@@ -366,7 +367,7 @@ export default function NavigationLayout({
                     onClick={handleClick}
                   >
                     <Icon>account_circle</Icon>
-                    {`${t(title)}`}
+                    {`${t("User number", { number: title.split(" ")[1] })}`}
                     <Icon>arrow_drop_down</Icon>
                   </Fab>
                   <Popover
@@ -424,7 +425,8 @@ export default function NavigationLayout({
                 title !== "Administrator" &&
                 title !== "User Administrator" &&
                 title !== "Practice Lead" &&
-                !title.startsWith("User") && (
+                !title.startsWith("Patient") &&
+                !!changeResearcherType && (
                   <Box>
                     <ModeToggleButton changeResearcherType={changeResearcherType} />
                   </Box>
@@ -432,7 +434,7 @@ export default function NavigationLayout({
             </Toolbar>
           )}
           {((authType !== "researcher" && authType !== "admin") ||
-            ((authType === "researcher" || authType === "admin") && title.startsWith("User"))) && (
+            ((authType === "researcher" || authType === "admin") && title.startsWith("Patient"))) && (
             <Toolbar
               classes={{
                 root:
@@ -440,7 +442,7 @@ export default function NavigationLayout({
                   (authType === "researcher" || authType === "admin" ? " " + classes.logToolbarResearcher : ""),
               }}
             >
-              {((authType !== "admin" && dashboardMenus.indexOf(activeTab) < 0) || title.startsWith("User")) && (
+              {((authType !== "admin" && dashboardMenus.indexOf(activeTab) < 0) || title.startsWith("Patient")) && (
                 <Container className={classes.thumbContainer}>
                   <Typography
                     variant="h5"
@@ -453,14 +455,14 @@ export default function NavigationLayout({
                   </Typography>
                 </Container>
               )}
-              {((authType !== "admin" && !sameLineTitle && activeTab !== "Studies") || title.startsWith("User")) && (
+              {((authType !== "admin" && !sameLineTitle && activeTab !== "Studies") || title.startsWith("Patient")) && (
                 <Container className={classes.thumbContainer}>
                   <Typography
                     variant="h5"
                     style={{
                       textTransform: "capitalize",
                       marginLeft:
-                        supportsSidebar && typeof title != "undefined" && title.startsWith("User") ? 0 : undefined,
+                        supportsSidebar && typeof title != "undefined" && title.startsWith("Patient") ? 0 : undefined,
                     }}
                   >
                     {typeof activeTab === "string" ? t(activeTab?.charAt(0).toUpperCase() + activeTab?.slice(1)) : ""}
@@ -470,6 +472,7 @@ export default function NavigationLayout({
               <Box flexGrow={1} />
               {typeof title != "undefined" &&
                 title.startsWith("User") &&
+                title !== "User Administrator" &&
                 (supportsSidebar || dashboardMenus.indexOf(activeTab) >= 0) && (
                   <Box className={classes.headerRight}>
                     {hideNotifications.indexOf(activeTab) < 0 ? (
@@ -490,7 +493,7 @@ export default function NavigationLayout({
                     )}
                   </Box>
                 )}
-              {typeof title != "undefined" && title.startsWith("User") && (
+              {typeof title != "undefined" && title.startsWith("User") && title !== "User Administrator" && (
                 <Box>
                   <Tooltip title={`${t("Profile & Settings")}`}>
                     <IconButton
@@ -509,7 +512,7 @@ export default function NavigationLayout({
                     onClose={() => setShowCustomizeMenu(undefined)}
                   >
                     <MenuItem disabled divider>
-                      <b>{`${t(title)}`}</b>
+                      <b>{`${t("User number", { number: title.split(" ")[1] })}`}</b>
                     </MenuItem>
                     <MenuItem divider onClick={() => setConfirmLogout(true)}>
                       {`${t("Logout")}`}
@@ -570,7 +573,7 @@ export default function NavigationLayout({
               classes.scroll +
               ((authType === "researcher" || authType === "admin") &&
               typeof title != "undefined" &&
-              title.startsWith("User")
+              title.startsWith("Patient")
                 ? " " + classes.logParticipantBorder
                 : authType === "researcher" || authType === "admin"
                 ? " " + classes.logResearcherBorder
