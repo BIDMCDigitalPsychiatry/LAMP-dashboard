7620869e541c88588a3b8e29d973abaf77c6d719
ITHQ1947\saritha.pillai <saritha.pillai@zcocorporation.com>
2023-08-10 17:28:26 +0530

    pop the bubbles bug fix - null in temporal slices

--- src/components/EmbeddedActivity.tsx
+++ src/components/EmbeddedActivity.tsx
@@ -186,6 +186,13 @@ export default function EmbeddedActivity({ participant, activity, name, onComple
     const exist = localStorage.getItem("first-time-" + (participant?.id ?? participant) + "-" + currentActivity?.id)
     try {
       setSaved(false)
+      console.log({
+        ...settings,
+        activity: currentActivity,
+        configuration: { language: i18n.language },
+        autoCorrect: !(exist === "true"),
+        noBack: noBack,
+      })
       setSettings({
         ...settings,
         activity: currentActivity,
@@ -214,6 +221,8 @@ export default function EmbeddedActivity({ participant, activity, name, onComple
     if (!!demoActivities[currentActivity.spec]) {
       let activityURL = "https://raw.githubusercontent.com/BIDMCDigitalPsychiatry/LAMP-activities/"
       activityURL += process.env.REACT_APP_GIT_SHA === "dev" ? "dist/out" : "latest/out"
+      return atob(await (await fetch(`${demoActivities[currentActivity.spec]}.html.b64`)).text())
+
       return atob(await (await fetch(`${activityURL}/${demoActivities[currentActivity.spec]}.html.b64`)).text())
     } else {
       return "about:blank"
