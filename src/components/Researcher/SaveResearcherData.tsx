23b5b0b16e4bd1066af89f598483294c21cd0487
ITHQ1947\saritha.pillai <saritha.pillai@zcocorporation.com>
2024-03-20 12:31:48 +0530

    Delete participant account - bug fixes

--- src/components/Researcher/SaveResearcherData.tsx
+++ src/components/Researcher/SaveResearcherData.tsx
@@ -118,7 +118,6 @@ export const saveDataToCache = (authString, id) => {
       "'sensors':[$map($LAMP.Sensor.list($study.id),function($sensor){{'name': " +
       " $sensor.name,'id':$sensor.id,'spec': $sensor.spec,'study_id': $study.id,'study_name': $study.name}})]}})]})"
   ).then((data: any) => {
-    console.log(data)
     let studies = Object.values(data?.studies || []).map((study: StudyObject) => {
       return {
         id: study?.id || "",
