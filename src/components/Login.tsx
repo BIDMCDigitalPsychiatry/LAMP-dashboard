3015c58406638617187eca81b01e8993c1f8c440
ITHQ1947\saritha.pillai <saritha.pillai@zcocorporation.com>
2025-06-12 15:48:09 +0530

    Module fixes

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -96,6 +96,7 @@ export default function Login({ setIdentity, lastDomain, onComplete, setConfirmS
   const [isLockedOut, setIsLockedOut] = useState(false)
   useEffect(() => {
     setConfirmSession(false)
+    localStorage.clear()
     const lockoutTime = localStorage.getItem(LOCKOUT_TIME_KEY)
     if (lockoutTime) {
       const lockoutEnd = parseInt(lockoutTime) + LOCKOUT_DURATION
